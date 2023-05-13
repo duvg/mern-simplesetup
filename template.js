8f46646323fd3d8d0ad01b427b0b658c187b5151
Duviel Garcia <duviel7@gmail.com>
2023-05-13 15:27:47 -0500

    Id 2: added configuration for server and client side

---
+++ template.js
@@ -0,0 +1,13 @@
+export default () => {
+  return `<!DOCTYPE html>
+    <html land="en">
+    <head>
+      <meta charset="utf-8">
+      <title>MERN Kickstart</title>
+    </head>
+    <body>
+      <div id="root"></div>
+      <script type="text/javascript" src="/dist/bundle.js"></script>
+    </body>
+    </html>`
+}
